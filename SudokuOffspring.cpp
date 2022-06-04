c4e20e85d2eb404b9c7b71a8d01bae335d0335fc
Tammy Le <tammyhle@uw.edu>
2022-06-03 20:33:30 -0700

    More debugging

--- SudokuOffspring.cpp
+++ SudokuOffspring.cpp
@@ -12,9 +12,9 @@ SudokuOffspring::~SudokuOffspring(){}
 
 //helper function to retreive probability
 bool SudokuOffspring::getProbability() {
-    int random = rand() % 100 + 1;
+    int random = rand() % 1000 + 1;
     /* cout << "random: " << random << " change? " << (random <= 50) << endl; */
-    return random <= 3;
+    return random <= 2;
 }
 
 // method to make offspring 
@@ -29,11 +29,12 @@ shared_ptr<Puzzle> SudokuOffspring::makeOffspring(shared_ptr<Puzzle>& p) {
 
     for (int i = 0; i < size + 1; i++) {
         for (int j = 0; j < size + 1; j++ ) {
-            if(!p->isFixed(i, j) && getProbability()) { //isFixed coming from yohanes sudoku class probability
+            if(!p->isFixed(i, j)) { //isFixed coming from yohanes sudoku class probability
                 // then change value
-                int random = rand() % 10;
-                /* cout << "value changed: " << p->grid[i][j] << " to: " << random << endl; */
-                copy->grid[i][j] = random; // adding 48 to correlate with ASCII 0 - 9
+                if(copy->grid[i][j] == 0 || getProbability() ){
+                    int random = rand() % 10;
+                    copy->grid[i][j] = random; // adding 48 to correlate with ASCII 0 - 9
+                }
             }
         }
     }
