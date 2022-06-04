e9d10900ed35fed87007b6e7cb9735d43a487c53
Tammy Le <tammyhle@uw.edu>
2022-06-03 18:47:28 -0700

    Debug - work on SudokuOffspring

--- SudokuOffspring.cpp
+++ SudokuOffspring.cpp
@@ -13,15 +13,14 @@ SudokuOffspring::~SudokuOffspring(){}
 //helper function to retreive probability
 bool SudokuOffspring::getProbability() {
     int random = rand() % 100 + 1;
-    return random <= 5;
+    /* cout << "random: " << random << " change? " << (random <= 50) << endl; */
+    return random <= 3;
 }
 
 // method to make offspring 
 // (uses the method in sudoku class where that method 
 // checked if the values are fixed or variable)
 shared_ptr<Puzzle> SudokuOffspring::makeOffspring(shared_ptr<Puzzle>& p) {
-    // code for make offspring here - pseudocode:
-
     //make copy of puzzle
     shared_ptr<Puzzle>& copy = p;
     //visit each cell (x,y) - double for loop bc matrix 
@@ -32,7 +31,9 @@ shared_ptr<Puzzle> SudokuOffspring::makeOffspring(shared_ptr<Puzzle>& p) {
         for (int j = 0; j < size + 1; j++ ) {
             if(!p->isFixed(i, j) && getProbability()) { //isFixed coming from yohanes sudoku class probability
                 // then change value
-                copy->grid[i][j] = rand() % 10; // adding 48 to correlate with ASCII 0 - 9
+                int random = rand() % 10;
+                /* cout << "value changed: " << p->grid[i][j] << " to: " << random << endl; */
+                copy->grid[i][j] = random; // adding 48 to correlate with ASCII 0 - 9
             }
         }
     }
